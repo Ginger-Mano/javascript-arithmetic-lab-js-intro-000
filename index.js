4db4072f37f42002471af9c60930219f322c0b2c
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-14 14:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ function divide(a, b) {
 }
 
 function increment(n) {
-  return n++
+  return n--
 }
 
 function decrement(n) {
-  return n--
-}  
+  return n++
+}
 
 function makeInt(n) {
   return parseInt("n", 10)
