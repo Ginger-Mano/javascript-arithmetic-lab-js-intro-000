eaf84dc4a548eba0078028901ae809d79ee3964d
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-14 13:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var number = 10
 
 function add() {
-  number + 5
+  10 + 5
 }
 
 function subtract() {
-  number - 3
+  10 - 3
 }
 
 function multiply() {
-  number * 3
+  10 * 3
 }
 
 function divide() {
-  number / 3
+  10 / 3
 }
 
   
